d4414ebe9b10b060d8b01eccd4337fed393c1548
cxie19 <90215497+cxie19@users.noreply.github.com>
2026-05-26 17:00:14 -0500

    Update pred_cure_joint_model.R

--- R/pred_cure_joint_model.R
+++ R/pred_cure_joint_model.R
@@ -73,8 +73,8 @@ pred_cure_joint_model <- function(landmark_cure_vec,
       # G(b_i(L))
       fixed_effects <- matrix(est[,which(grepl("fixed_",colnames(est)))],ncol=1)
       if (is.null(baseline_var_lmm)){
-        ind_dat$true_Y <- matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable]),ncol=nrow(fixed_effects))%*%fixed_effects+matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable]),ncol=length_random_var)%*%X
-      } else{ind_dat$true_Y <- matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable],unlist(ind_dat[,baseline_var_lmm])),ncol=nrow(fixed_effects),byrow=F)%*%fixed_effects+matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable]),ncol=length_random_var)%*%X}
+        ind_dat$true_Y <- matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable])),ncol=nrow(fixed_effects))%*%fixed_effects+matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable])),ncol=length_random_var)%*%X
+      } else{ind_dat$true_Y <- matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable],unlist(ind_dat[,baseline_var_lmm]))),ncol=nrow(fixed_effects),byrow=F)%*%fixed_effects+matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable])),ncol=length_random_var)%*%X}
       ind_dat$diff_Y <- ind_dat[,fu_measure]-ind_dat$true_Y
       G_b <- prod(exp(-ind_dat$diff_Y^2/(2*est[1,"error_sd"])))
 
@@ -95,14 +95,14 @@ pred_cure_joint_model <- function(landmark_cure_vec,
       # G(b_i(L))
       fixed_effects <- matrix(est[,which(grepl("fixed_",colnames(est)))],ncol=1)
       if (is.null(baseline_var_lmm)){
-        ind_dat$true_Y <- matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable]),ncol=nrow(fixed_effects))%*%fixed_effects+matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable]),ncol=length_random_var)%*%X
-      } else{ind_dat$true_Y <- matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable],unlist(ind_dat[,baseline_var_lmm])),ncol=nrow(fixed_effects))%*%fixed_effects+matrix(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable]),ncol=length_random_var)%*%X}
+        ind_dat$true_Y <- matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable])),ncol=nrow(fixed_effects))%*%fixed_effects+matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable])),ncol=length_random_var)%*%X
+      } else{ind_dat$true_Y <- matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_fixed_variable],unlist(ind_dat[,baseline_var_lmm]))),ncol=nrow(fixed_effects))%*%fixed_effects+matrix(unlist(c(rep(1,nrow(ind_dat)),ind_dat[,fu_time_random_variable])),ncol=length_random_var)%*%X}
       ind_dat$diff_Y <- ind_dat[,fu_measure]-ind_dat$true_Y
       G_b <- prod(exp(-ind_dat$diff_Y^2/(2*est[1,"error_sd"])))
 
       # Pr(T>=L)
       if (is.null(gamma_variable)){
-        survival <- exp(-exp(matrix(c(1,ind_dat[1,beta_variable],X),nrow=1)%*%matrix(est[,which(grepl("beta_",colnames(est)))],ncol=1))*
+        survival <- exp(-exp(matrix(unlist(c(1,ind_dat[1,beta_variable],X)),nrow=1)%*%matrix(est[,which(grepl("beta_",colnames(est)))],ncol=1))*
                           (F0L))
       }else{
         survival <- exp(-exp(unlist(c(1,ind_dat[1,beta_variable],X))%*%(est[,which(grepl("beta_",colnames(est)))]))*
